a18d8d73ac9bfdad8b39d978d8d190ffa489520f
Anil Kumar Lenka <59606027+anil-kumar-lenka@users.noreply.github.com>
2020-03-11 23:06:32 +0530

    New Script Folder Created

--- ResumeBuilder/ResumeBuilder/Controllers/PublicProfileController.cs
+++ ResumeBuilder/ResumeBuilder/Controllers/PublicProfileController.cs
@@ -1,8 +1,5 @@
 ﻿using ResumeBuilder.Models;
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace ResumeBuilder.Controllers
@@ -21,16 +18,26 @@ namespace ResumeBuilder.Controllers
         {
             // User Name
             ViewBag.profName = _context.Users.FirstOrDefault(a => a.UserID == id).Name;
+
             // User Role
             ViewBag.profRole = "Web Developer";
+            
             // User Phone
             ViewBag.profPhone = _context.Users.FirstOrDefault(a => a.UserID == id).PhoneNumber;
+
             // User E-mail
             ViewBag.profEmail = _context.Users.FirstOrDefault(a => a.UserID == id).Username;
+
             //User Linkedin Link
             ViewBag.profLinkedin = "https://www.linkedin.com/user";
+
             // User Summary
             ViewBag.profSummary = "Oh, I misunderstood the problem. ResumeBuilder ResumeBuilder Setting a padding on, ResumeBuilder ResumeBuilder bin the padding won't help you.";
+            
+            //Education Details
+            ViewBag.education = 1;
+            
+            
             return View();
             //var data = _context.Users.ToList(),return Json(data, JsonRequestBehavior.AllowGet);
         }
---
+++ ResumeBuilder/ResumeBuilder/Scripts/Profile_Scripts/profileDefaultScript.js
@@ -0,0 +1,21 @@
+﻿// JavaScript file for Public Profile Page
+$(document).ready(function () {
+
+    var onResize = function () {
+        // apply dynamic padding at the top of the body according to the fixed navbar height
+        $("body").css("padding-top", $(".fixed-top").height());
+    };
+
+    // attach the function to the window resize event
+    $(window).resize(onResize);
+
+    // call it also when the page is ready after load or reload
+    $(function () {
+        onResize();
+    });
+
+    if (ViewBag.education)
+        console.log('Inside Js');
+    else
+        console.log('Outside Js');
+});
