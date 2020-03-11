caa64a9c3b4a751bbed6b9ed9b0f789c46be0805
Nirlipta13 <51902470+Nirlipta13@users.noreply.github.com>
2020-03-11 16:54:38 +0530

    ajax call for summary field

--- ResumeBuilder/ResumeBuilder/Controllers/ResumeController.cs
+++ ResumeBuilder/ResumeBuilder/Controllers/ResumeController.cs
@@ -9,6 +9,11 @@ namespace ResumeBuilder.Controllers
 {
     public class ResumeController : Controller
     {
+        private ResumeBuilderConnection db;
+        public ResumeController()
+        {
+            db = new ResumeBuilderConnection();
+        }
         // GET: Resume
         public ActionResult Index()
         {
@@ -18,9 +23,20 @@ namespace ResumeBuilder.Controllers
         public ActionResult Form(User user)
         {
             if (Session["UserID"] != null)
+
                 return View(user);
 
             return RedirectToAction("Login","Account");
+        }
+
+        [HttpPost]
+        public ActionResult SaveBasicInformation(User user)
+        {
+            var userFromDb=db.Users.FirstOrDefault(u=>u.UserID==user.UserID);
+            userFromDb.Summary = user.Summary;
+            db.SaveChanges();
+            string message = "SUCCESS";
+            return Json(new { Message = message, JsonRequestBehavior.AllowGet });
         }
     }
 }
