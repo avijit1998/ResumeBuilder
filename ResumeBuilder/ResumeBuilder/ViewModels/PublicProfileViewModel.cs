61ed618989ecdf7e04995ef5ac6a64a8f6bdfb30
Anil Kumar Lenka <59606027+anil-kumar-lenka@users.noreply.github.com>
2020-03-16 15:46:11 +0530

    Education And Language accessing from DB

--- ResumeBuilder/ResumeBuilder/ViewModels/PublicProfileViewModel.cs
+++ ResumeBuilder/ResumeBuilder/ViewModels/PublicProfileViewModel.cs
@@ -11,22 +11,24 @@ namespace ResumeBuilder.ViewModels
         public string LinkedinLink { get; set; }
         public string UserRole { get; set; }
         public string Summary { get; set; }
-        public List<string> Languages { get; set; }
+        // Language
+        public int LanguageStatus { get; set; }
+        public List<IEnumerable<string>> Languages { get; set; }
 
         // Educational Details
-        public bool EducationStatus { get; set; }
+        public int EducationStatus { get; set; }
         public List<EducationUIModel> EducationList { get; set; }
 
         // Skills
-        public bool SkillStatus { get; set; }
-        public List<string> SkillList { get; set; }
+        public int SkillStatus { get; set; }
+        public List<IEnumerable<string>> SkillList { get; set; }
 
         // Projects
-        public bool ProjectStatus { get; set; }
+        public int ProjectStatus { get; set; }
         public List<ProjectUIModel> ProjectList { get; set; }
 
         // Work Experience
-        public bool WorkExStatus { get; set; }
+        public int WorkExStatus { get; set; }
         public List<WorkExUIModel> WorkExList { get; set; }
 
         // Contact Details
