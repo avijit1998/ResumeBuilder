3f36e855088f56bf851ec0185274ee8c51b4046b
Avijit Nanda <avijitnanda14@gmail.com>
2020-03-27 14:04:41 +0530

    some changes

--- ResumeBuilder/ResumeBuilder/Controllers/AccountController.cs
+++ ResumeBuilder/ResumeBuilder/Controllers/AccountController.cs
@@ -1,13 +1,6 @@
 ﻿using System;
-using System.Globalization;
 using System.Linq;
-using System.Security.Claims;
-using System.Threading.Tasks;
-using System.Web;
 using System.Web.Mvc;
-using Microsoft.AspNet.Identity;
-using Microsoft.AspNet.Identity.Owin;
-using Microsoft.Owin.Security;
 using ResumeBuilder.Models;
 using System.Security.Cryptography;
 using ResumeBuilder.ViewModels;
