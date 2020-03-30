156795bab8a62a1cf35248a15515259b8fb61a52
Nirlipta13 <51902470+Nirlipta13@users.noreply.github.com>
2020-03-30 20:46:19 +0530

    added view models and minimised db fetch and cretaed common ajax

---
+++ ResumeBuilder/ResumeBuilder/ViewModels/ProjectInfoVM.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ResumeBuilder.ViewModels
+{
+    public class ProjectInfoVM
+    {
+        public int ProjectID { get; set; }
+
+        public int UserID { get; set; }
+
+        public string ProjectTitle { get; set; }
+
+        public int DurationInMonth { get; set; }
+
+        public string ProjectRole { get; set; }
+
+        public string Description { get; set; }
+
+    }
+}
