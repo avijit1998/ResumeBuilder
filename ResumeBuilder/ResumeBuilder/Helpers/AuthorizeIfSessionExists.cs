9a82c8b5759032fbb2cb4f02a423526a9ce11a8e
Avijit Nanda <avijitnanda14@gmail.com>
2020-04-01 01:12:33 +0530

    fixed logout section

---
+++ ResumeBuilder/ResumeBuilder/Helpers/AuthorizeIfSessionExists.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ResumeBuilder.Helpers
+{
+
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true, Inherited = true)]
+    public class AuthorizeIfSessionExistsAttribute : AuthorizeAttribute, IAuthorizationFilter
+    {
+        protected override bool AuthorizeCore(HttpContextBase httpContext)
+        {
+            var sessionUserID = httpContext.Session["UserID"];
+            
+            if (sessionUserID != null)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
+        
+        
