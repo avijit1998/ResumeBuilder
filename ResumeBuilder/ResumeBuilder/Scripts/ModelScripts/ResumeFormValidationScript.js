0c0ab2733bcf2747eeae1f2994e9070957c14a3b
Rahulk101 <59678033+Rahulk101@users.noreply.github.com>
2020-04-02 15:38:04 +0530

    Education details validation and rectified error in register script.

--- ResumeBuilder/ResumeBuilder/Scripts/ModelScripts/ResumeFormValidationScript.js
+++ ResumeBuilder/ResumeBuilder/Scripts/ModelScripts/ResumeFormValidationScript.js
@@ -51,13 +51,68 @@ $(document).ready(function () {
     })
 
     //Rahul
-    $("body").on('click', '.js-save-education', function () {
-        $('#modalEducationDetails').validate({
-            rules: {
+    $("body").on('click', '.js-add-education,.js-save-education, .js-edit-education', function () {
+        $("#modalEducationDetails").on('shown.bs.modal', function () {
 
-            }
+            $.validator.addMethod("regex", function (value, element, regexpr) {
+                return this.optional(element) || regexpr.test(value);
+            }, "Invalid input.");
+
+
+
+            $('#educationDetailsForm').validate({
+                rules: {
+                    //courseOption: {
+                    //    required: function () {
+                    //        var course1 = $("#course1").prop('checked');
+                    //        var course2 = $("#course2").prop('checked');
+                    //        var course3 = $("#course3").prop('checked');
+                    //        var course4 = $("#course4").prop('checked');
+                    //        if (!(course1 || course2 || course3 || course4))
+                    //            return true;
+                    //        return false;
+                    //    }
+                    //},
+                    Stream: {
+                        required: true,
+                        regex: /^[A-Za-z ]+$/
+                    },
+                    PassingYear: {
+                        required: true,
+                        regex: /^[12][0-9]{3}$/
+                    },
+                    TotalPercentageOrCGPAValue: {
+                        required: true,
+                        regex: /^(([0]|[0-9]\.(\d?\d?)|[10].[0])|(100$|^\d{0,2}(\.\d{1,2})? *%?))$/
+                    }
+                },
+                messages: {
+                    //courseOption: {
+                    //    required: "Please check your Course first."
+                    //},
+                    Stream: {
+                        required: "Enter your Stream.",
+                        regex: "Enter valid Stream."
+                    },
+                    PassingYear: {
+                        required: "Enter your passing year.",
+                        regex: "Enter valid year of passing."
+                    },
+                    TotalPercentageOrCGPAValue: {
+                        required: function () {
+                            var cgpa = $("#cgpa").prop('checked');
+
+                            if (cgpa)
+                                return "Enter CGPA.";
+                            return "Enter Percentage.";
+                        },
+                        regex: "Enter valid value."
+                    }
+                }
+            })
         })
-    })
+
+    });
 });
 
 
